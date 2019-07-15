54a9d4ee325162699d75489471c52a1dc46e9b02
Yuya OTA <yuya.gt@gmail.com>
2019-07-15 13:25:09 +0900

    Separate transformPB function

--- api/scrum_team.go
+++ api/scrum_team.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 
+	"github.com/tennashi/semer/domain"
 	"github.com/tennashi/semer/proto/gen/data/v1"
 	"github.com/tennashi/semer/proto/gen/services/scrum_team/v1"
 	"github.com/tennashi/semer/usecase"
@@ -16,6 +17,29 @@ func NewScrumTeamServer() scrum_team.ScrumTeamServer {
 	return &scrumTeam{}
 }
 
+func transformPB(st *domain.ScrumTeam) *data.ScrumTeam {
+	prodOwner := &data.User{
+		Id: st.ProdOwner.ID,
+	}
+	scrumMaster := &data.User{
+		Id: st.ScrumMaster.ID,
+	}
+	devTeam := make([]*data.User, len(st.DevTeam))
+	for _, member := range st.DevTeam {
+		m := &data.User{
+			Id: member.ID,
+		}
+		devTeam = append(devTeam, m)
+	}
+
+	return &data.ScrumTeam{
+		Id:          st.ID,
+		ProdOwner:   prodOwner,
+		ScrumMaster: scrumMaster,
+		DevTeam:     devTeam,
+	}
+}
+
 func (st *scrumTeam) FindTeamByID(
 	ctx context.Context,
 	req *scrum_team.FindTeamByIDRequest,
@@ -25,28 +49,9 @@ func (st *scrumTeam) FindTeamByID(
 		return nil, err
 	}
 
-	prodOwner := &data.User{
-		Id: team.ProdOwner.ID,
-	}
-	scrumMaster := &data.User{
-		Id: team.ScrumMaster.ID,
-	}
-	devTeam := []*data.User{}
-	for _, member := range team.DevTeam {
-		m := &data.User{
-			Id: member.ID,
-		}
-		devTeam = append(devTeam, m)
-	}
-
-	scrumTeam := &data.ScrumTeam{
-		Id:          1,
-		ProdOwner:   prodOwner,
-		ScrumMaster: scrumMaster,
-		DevTeam:     devTeam,
-	}
+	res := transformPB(team)
 
 	return &scrum_team.FindTeamByIDResponse{
-		Team: scrumTeam,
+		Team: res,
 	}, nil
 }
